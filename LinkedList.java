c9a05061182f844d11059cefdf759e90c7726d3e
FaridCG343 <71570619+FaridCG343@users.noreply.github.com>
2022-10-15 16:56:19 -0600

    Create LinkedList.java

---
+++ LinkedList.java
@@ -0,0 +1,80 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.linkedlist;
+
+/**
+ *
+ * @author farid
+ * @param <E>
+ */
+public class LinkedList <E>{
+
+    public Nodo i, f;
+
+    public LinkedList() {
+        this.i = null;
+        this.f = null;
+    }
+
+    public void insertarF(E dato) {
+        Nodo q = new Nodo(dato);
+        if (i == null) {
+            i = q;
+            f = q;
+        } else {
+            if (i.siguiente == null) {
+                i.siguiente = q;
+                q.anterior = i;
+                f = q;
+            } else {
+                f.siguiente = q;
+                q.anterior = f;
+                f = q;
+            }
+        }
+    }
+
+    public void insertarI(E dato){
+        Nodo q = new Nodo(dato);
+        if (i == null) {
+            i = q;
+            f = q;
+        } else {
+            i.anterior = q;
+            q.siguiente = i;
+            i = q;
+        }
+    }
+    
+    public String recorrer() {
+        String cadena = "";
+        Nodo aux = i;
+        while (aux != null) {
+            cadena += aux.dato + " ";
+            aux = aux.siguiente;
+        }
+        return cadena;
+    }
+
+    public String recorrerInv() {
+        String cadena = "";
+        Nodo aux = f;
+        while (aux != null) {
+            cadena += aux.dato + " ";
+            aux = aux.anterior;
+        }
+        return cadena;
+    }
+    
+    public void eliminarInicio(){
+        i = i.siguiente;
+    }
+    
+    public void eliminarFinal(){
+        f = f.anterior;
+        f.siguiente = null;
+    }
+    
+}
