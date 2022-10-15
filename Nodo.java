895dd533f88e1c9c5acdae067ecc91a67df989e4
FaridCG343 <71570619+FaridCG343@users.noreply.github.com>
2022-10-15 16:56:58 -0600

    Create Nodo.java

---
+++ Nodo.java
@@ -0,0 +1,12 @@
+public class Nodo <E>{
+    public Nodo anterior,siguiente;
+    public E dato;
+
+    public Nodo(E dato) {
+        this.anterior = null;
+        this.siguiente = null;
+        this.dato = dato;
+    }
+    
+    
+}
