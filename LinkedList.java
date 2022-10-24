8b2e62a075799ba1135d3c41ff968ad76a54c7ed
FaridCG343 <71570619+FaridCG343@users.noreply.github.com>
2022-10-24 12:34:39 -0600

    feat: some feature added
    Delete and get values at some index
    get length

--- LinkedList.java
+++ LinkedList.java
@@ -1,10 +1,22 @@
-public class LinkedList <E>{
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+package com.mycompany.linkedlist;
 
-    public Nodo i, f;
+/**
+ *
+ * @author 701
+ */
+public class LinkedList<E> {
+
+    private Nodo i, f;
+    private int length;
 
     public LinkedList() {
         this.i = null;
         this.f = null;
+        this.length = 0;
     }
 
     public void insertarF(E dato) {
@@ -12,31 +24,36 @@ public class LinkedList <E>{
         if (i == null) {
             i = q;
             f = q;
+            length++;
         } else {
             if (i.siguiente == null) {
                 i.siguiente = q;
                 q.anterior = i;
                 f = q;
+                length++;
             } else {
                 f.siguiente = q;
                 q.anterior = f;
                 f = q;
+                length++;
             }
         }
     }
 
-    public void insertarI(E dato){
+    public void insertarI(E dato) {
         Nodo q = new Nodo(dato);
         if (i == null) {
             i = q;
             f = q;
+            length++;
         } else {
             i.anterior = q;
             q.siguiente = i;
             i = q;
+            length++;
         }
     }
-    
+
     public String recorrer() {
         String cadena = "";
         Nodo aux = i;
@@ -56,14 +73,58 @@ public class LinkedList <E>{
         }
         return cadena;
     }
-    
-    public void eliminarInicio(){
-        i = i.siguiente;
+
+    public void eliminarInicio() {
+        if (i != null) {
+            i = i.siguiente;
+            length--;
+        }
     }
-    
-    public void eliminarFinal(){
-        f = f.anterior;
-        f.siguiente = null;
+
+    public void eliminarFinal() {
+        if (length != 0) {
+            f = f.anterior;
+            f.siguiente = null;
+            length--;
+        }
     }
-    
+
+    public E getI() {
+        return (E) i.dato;
+    }
+
+    public E getF() {
+        return (E) f.dato;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public E getAt(int posicion) {
+        if (posicion < 0 || posicion > length) {
+            return null;
+        }
+        Nodo aux = i;
+        while (posicion != 1) {
+            posicion--;
+            aux = aux.siguiente;
+        }
+        return (E) aux.dato;
+    }
+
+    public void popAt(int posicion) {
+        if (posicion < 0 || posicion > length) {
+            return;
+        }
+        Nodo aux = i;
+        while (posicion != 1) {
+            posicion--;
+            aux = aux.siguiente;
+        }
+        Nodo ant = aux.anterior;
+        Nodo sig = aux.siguiente;
+        ant.siguiente = sig;
+        sig.anterior = ant;
+    }
 }
