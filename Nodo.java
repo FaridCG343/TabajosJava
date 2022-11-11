bb57abb58a995ef8b68529b991a878899022dd65
FaridCG343 <71570619+FaridCG343@users.noreply.github.com>
2022-11-10 21:54:13 -0700

    Update Nodo.java

--- Nodo.java
+++ Nodo.java
@@ -10,3 +10,27 @@ public class Nodo <E>{
     
     
 }
+
+public class Nodo <E>{
+    public E dato;
+    public Nodo izq, der;
+
+    public Nodo() {
+        dato = null;
+        izq = null;
+        der = null;
+    }
+
+    public Nodo(E dato) {
+        this.dato = dato;
+        izq = null;
+        der = null;
+    }
+
+    @Override
+    public String toString() {
+        return (String)dato;
+    }
+    
+    
+}
