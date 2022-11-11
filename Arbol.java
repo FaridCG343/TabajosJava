f5b5c0bf2e395686f545d43e57998869f95392d4
FaridCG343 <71570619+FaridCG343@users.noreply.github.com>
2022-11-10 21:35:48 -0700

    Update Arbol.java

--- Arbol.java
+++ Arbol.java
@@ -1,21 +1,97 @@
-//Metodos para recorrer pos y pre
-  public void recorrerPre(Nodo Raiz){
-        System.out.print(Raiz + " ");
-        if(Raiz.getLigaI()!=null){
-            recorrerPre(Raiz.getLigaI());
+public class Arbol{
+    private Nodo<String> R;
+    private int altura;
+    
+    public Arbol(){
+        R = null;
+        altura = 0;
+    }
+    
+    public Arbol(String dato){
+        R = new Nodo<>(dato);
+        altura = 1;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+    
+    public void insertar(String dato){
+        if(R == null){
+            R = new Nodo<>(dato);
+            altura = 1;
+            return;
         }
-        if(Raiz.getLigaD()!=null){
-            recorrerPre(Raiz.getLigaD());
+        insertarString(R, dato, 1);
+    }
+    
+    private void insertarString(Nodo R, String dato, int a){
+        a++;
+        if(dato.compareToIgnoreCase(R.toString())==0){
+            return;
+        }
+        System.out.println(a);
+        if(dato.compareToIgnoreCase(R.toString())>0){ //Mayor
+            if(R.der ==null){
+                Nodo<String> Q = new Nodo<>(dato);
+                R.der = Q;
+                altura = a>altura?a:altura;
+                return;
+            }
+            insertarString(R.der, dato, a);
+        }else{ //Menor
+            if(R.izq ==null){
+                Nodo<String> Q = new Nodo<>(dato);
+                R.izq = Q;
+                altura = a>altura?a:altura;
+                return;
+            }
+            insertarString(R.izq, dato, a);
+        }
+    }
+    
+    public void recorrerIn() {
+        if(R==null)return;
+        In(R);
+    }
+    
+    public void recorrerPre(){
+        if(R==null)return;
+        Pre(R);
+    }
+
+    public void recorrerPos(){
+        if(R==null)return;
+        Pos(R);
+    }
+    
+    private void In(Nodo Raiz) {
+        if (Raiz.izq != null) {
+            In(Raiz.izq);
+        }
+        System.out.print(Raiz + " ");
+        if (Raiz.der != null) {
+            In(Raiz.der);
+        }
+    }
+    
+    private void Pre(Nodo Raiz){
+        System.out.print(Raiz + " ");
+        if (Raiz.izq != null) {
+            Pre(Raiz.izq);
+        }
+        if (Raiz.der != null) {
+            Pre(Raiz.der);
         }
     }
 
-    public void recorrerPos(Nodo Raiz){
-        if(Raiz.getLigaI()!=null){
-            recorrerPos(Raiz.getLigaI());
+    private void Pos(Nodo Raiz){
+        if (Raiz.izq != null) {
+            Pos(Raiz.izq);
         }
-        if(Raiz.getLigaD()!=null){
-            recorrerPos(Raiz.getLigaD());
+        if (Raiz.der != null) {
+            Pos(Raiz.der);
         }
         System.out.print(Raiz + " ");
     }
-    
+}
